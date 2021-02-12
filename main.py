4a18398b0a1cc19544860e22debead51633ef03c
AppleP1e <78921034+AppleP1e@users.noreply.github.com>
2021-02-12 19:46:42 +0100

    'info' command

--- main.py
+++ main.py
@@ -20,18 +20,22 @@ def logo():
 
 generated = 0
 tries = 0
+mode = "practice"
+version = "update2"
+low = 0
+high = 1000
 
 
 def init(starts, stops, steps):
-    if starts is 0:
+    if starts == 0:
         begin = input("start number> ")
     else:
         begin = starts
-    if stops is 0:
+    if stops == 0:
         stop = input("end number> ")
     else:
         stop = stops
-    if steps is 0:
+    if steps == 0:
         step = input("step> ")
     else:
         step = steps
@@ -48,6 +52,12 @@ def start():
         difficulties()
     elif choice == "2":
         practice()
+    elif choice == "exit":
+        choice = input("Are you sure?[y/n]> ")
+        if choice == "y":
+            sys.exit(0)
+        else:
+            start()
     else:
         print("invalid input... quitting!")
         sys.exit(0)
@@ -56,6 +66,7 @@ def start():
 def difficulties():
     global tries
     global generated
+    global mode
     logo()
     print("You chose mode 1, preset...\nChoose the level of difficulty you want:")
     print("\n\n\n[1]Hard - 1-1000, 10 tries\n[2]Normal - 1-1000, 15 tries\n[3]Easy - 1-1000, 20 tries\n\n")
@@ -63,16 +74,27 @@ def difficulties():
     generated = init(1, 1000, 1)
     if command == "1":
         tries = 10
+        mode = "hard"
         print("chose hard difficulty... starting!")
         main()
     elif command == "2":
         tries = 15
+        mode = "medium"
         print("chose medium difficulty... starting!")
         main()
     elif command == "3":
         tries = 20
+        mode = "easy"
         print("chose easy difficulty... starting!")
         main()
+    elif command == "back":
+        start()
+    elif command == "exit":
+        choice = input("Are you sure?[y/n]> ")
+        if choice == "y":
+            sys.exit(0)
+        else:
+            difficulties()
     else:
         print("Invalid input... quitting!")
         sys.exit(0)
@@ -80,7 +102,9 @@ def difficulties():
 
 def practice():
     global tries
+    global mode
     global generated
+    mode = "practice"
     logo()
     print("you chose practice mode! you have infinite tries, and you can set whatever values you want!")
     generated = init(0, 0, 0)
@@ -105,6 +129,10 @@ def cmd():
 def main():
     while True:
         global tries
+        global version
+        global mode
+        global low
+        global high
         if tries > 0:
             command = cmd()
             if command == "help":
@@ -113,6 +141,7 @@ def main():
                 print("type 'help' to show this!")
                 print("type 'exit' to quit!")
                 print("type 'restart' to restart!")
+                print("type 'info' to get info about your game!")
             if str.isdigit(command):
                 if int(command) == int(generated):
                     os.system('cls' if os.name == 'nt' else 'clear')
@@ -121,21 +150,42 @@ def main():
                     break
                 if int(command) > int(generated):
                     print("lower!")
+                    if int(command) < int(high):
+                        high = int(command)
                     tries = tries - 1
                 if int(command) < int(generated):
                     print("higher!")
+                    if int(command) > int(low):
+                        low = int(command)
                     tries = tries - 1
             if command == "cls":
                 logo()
-                os.system('cls' if os.name == 'nt' else 'clear')
             if command == "exit":
-                sys.exit(0)
+                choice = input("Are you sure?[y/n]> ")
+                if choice == "y":
+                    sys.exit(0)
+                else:
+                    print("chose no...")
             if command == "restart":
                 choice = input("Are you sure?[y/n]> ")
                 if choice == "y":
                     start()
                 else:
                     print("chose no...")
+            if command == "info":
+                print("Version: " + version)
+                print("Current Mode: " + mode)
+                total = 0
+                if mode == "easy":
+                    total = 20
+                elif mode == "medium":
+                    total = 15
+                elif mode == "hard":
+                    total = 10
+                elif total == "practice":
+                    total = 9999999999
+                print("Tries; left: " + str(tries) + " used: " + str(total - tries) + " total: " + str(total))
+                print("known value; between " + str(low) + " and " + str(high))
         else:
             print("out of tries! better luck next time!")
             restart()
